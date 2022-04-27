91da8d966ac90c68400ef55d01fc6a40060a5e1b
CyberPim <54932011+CyberPim@users.noreply.github.com>
2022-04-27 15:44:13 +0200

    Call location from gui

--- Python code/location.py
+++ Python code/location.py
@@ -1,15 +1,25 @@
 # importing geopy library
 from geopy.geocoders import Nominatim
 
-# calling the Nominatim tool
-loc = Nominatim(user_agent="GetLoc")
+class Location():
 
-# entering the location name
-getLoc = loc.geocode("jopenkerk, haarlem")
+    # def __init__(self):
+    #     self.location()
 
-# printing address
-print(getLoc.address)
+    def location(self):
+        # calling the Nominatim tool
+        loc = Nominatim(user_agent="GetLoc")
 
-# printing latitude and longitude
-print("Latitude = ", getLoc.latitude)
-print("Longitude = ", getLoc.longitude)
+        # entering the location name
+        getLoc = loc.geocode("jopenkerk, haarlem")
+
+        # printing address
+        print(getLoc.address)
+
+        # printing latitude and longitude
+        print("Latitude = ", getLoc.latitude)
+        print("Longitude = ", getLoc.longitude)
+
+
+if __name__ == "__main__":
+    location = Location()
